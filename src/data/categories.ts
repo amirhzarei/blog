e0f4a52f9dc267c610797266cdf848aafed414dc
Amir Hossein Zarei <amirhzarei24107@yahoo.com>
2025-09-04 16:38:13 +0330

    feat: add new blogs sep 4

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,12 @@
 // List of categories for blog posts
-export const CATEGORIES = ['AI'] as const
+export const CATEGORIES = [
+	'AI',
+	'Development Techniques',
+	'React',
+	'Next Js',
+	'JavaScript',
+	'TypeScript',
+	'Tools',
+	'HTML',
+	'CSS'
+] as const
