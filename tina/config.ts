132e8dd3d9c8456da7630062405f419b58631ec8
Amir Hossein Zarei <amirhzarei24107@yahoo.com>
2025-10-01 19:49:32 +0330

    feat: update tina

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	branch,
-	clientId: null,
-	token: null,
+	clientId: process.env.TINA_CLIENT_ID,
+	token: process.env.TINA_TOKEN,
 
 	build: {
 		outputFolder: 'admin',
